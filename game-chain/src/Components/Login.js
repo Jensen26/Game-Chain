748d4de15b42ab0af437ec9b079bfc20ac79ad3f
Jensen26 <100305874+Jensen26@users.noreply.github.com>
2022-12-06 16:40:26 -0500

    Add files via upload

---
+++ game-chain/src/Components/Login.js
@@ -0,0 +1,88 @@
+import React from "react";
+import "./Login.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import { useUserAuth } from "../Context/UserAuthContext";
+import { useLogin } from "../Context/LoginProvider";
+
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login } = useUserAuth();
+  const { setIsLoggedIn } = useLogin();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      await login(email, password);
+      setIsLoggedIn(true);
+      navigate("/Profile");
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+
+  return (
+    <div className="Login">
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <h1>LOGIN</h1>
+      <br />
+      <form onSubmit={handleLogin}>
+        <input
+          className="INPUT"
+          placeholder="    Email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+
+        <input
+          className="INPUT"
+          placeholder="    Password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br />
+        <br />
+        <button className="LoginSignup">SIGN IN</button>
+        <br />
+        <br />
+        <br />
+        <p>
+          Don't have an account?
+          <br />
+          <Link className="ClickHere" to="/SignUp">
+            Click here
+          </Link>
+        </p>
+      </form>
+      <div></div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+    </div>
+  );
+}
+
+export default Login;
